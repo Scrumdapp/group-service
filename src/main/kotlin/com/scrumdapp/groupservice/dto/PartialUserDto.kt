d1d3bb9ebd7b2a0536c22f20658792d8ce35d3c7
StevenSeagull1 <stevenhoekstra2005@gmail.com>
2026-05-21 10:56:03 +0200

    oke nu is die wel goed

--- src/main/kotlin/com/scrumdapp/groupservice/dto/PartialUserDto.kt
+++ src/main/kotlin/com/scrumdapp/groupservice/dto/PartialUserDto.kt
@@ -3,8 +3,8 @@ package com.scrumdapp.groupservice.dto
 import jakarta.validation.constraints.Size
 
 data class PartialUserDto (
-    val userId: Long?,
-    val groupId: Long?,
+    val userId: Long,
+    val groupId: Long,
     @field:Size(min = 3, max = 64)
     val firstName: String?,
     @field:Size(min = 3, max = 64)
