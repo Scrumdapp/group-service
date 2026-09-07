1f2dc4d538b6099217a7b931b6ad4b2f934e6797
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-09-07 12:54:47 +0200

    Revert "adjusted group creation dto"
    This reverts commit 05cea4ebdd598701aa18f48976c7f31b87f97c7e.

--- src/main/kotlin/com/scrumdapp/groupservice/dto/CreateGroupDto.kt
+++ src/main/kotlin/com/scrumdapp/groupservice/dto/CreateGroupDto.kt
@@ -5,9 +5,9 @@ import jakarta.validation.constraints.Size
 data class CreateGroupDto(
     @field:Size(min = 3, message = "group name must be at least 3 characters", max = 64)
     @field:Pattern(
-        regexp = "^[a-zA-Z0-9-_#!?(),. ]*$",
-        message = "Only the characters a-z, A-Z, 0-9, -_#!>(),. are allowed"
+        regexp = "^[a-zA-Z0-9#!?]+([ ][a-zA-Z0-9#!?]+)*$",
+        message = "Only a-z, A-Z, 0-9, # ! ? allowed"
     )
-    val name: String,
+        val name: String,
     val backgroundPreference: Int?
 )
